30cef4f54959bf52a8280301eb26f18787b7ab40
tristan-guepeed <tristan.beau@epitech.eu>
2025-09-08 12:26:03 +0200

    [ADD] rest_framework settings

--- project_gestion/project_gestion/settings.py
+++ project_gestion/project_gestion/settings.py
@@ -45,6 +45,15 @@ INSTALLED_APPS = [
 
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
