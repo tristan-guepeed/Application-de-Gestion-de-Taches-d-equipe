94cf606e09d2007b372ecc7e1480c755361738ff
tristan-guepeed <tristan.beau@epitech.eu>
2025-09-08 12:38:26 +0200

    [ADD] register + login + refresh token

--- project_gestion/project_gestion/urls.py
+++ project_gestion/project_gestion/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     #path("api/projects/", include("projects.urls")),
     #path("api/tasks/", include("tasks.urls")),
-    #path("api/users/", include("users.urls")),
+    path("api/users/", include("users.urls")),
 ]
--- project_gestion/users/views.py
+++ project_gestion/users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
